0645f521cf926b0f7b9de7c4ef0f0b09b3ef7f82
fireman-developer <firemandeveloper25@gmail.com>
2026-01-03 12:12:02 +0330

    QrcodeViewModel error fix

--- v2rayN/v2rayN/Views/QrcodeView.xaml.cs
+++ v2rayN/v2rayN/Views/QrcodeView.xaml.cs
@@ -1,37 +1,13 @@
-using System.Reactive.Disposables;
 using System.Windows;
-using ReactiveUI;
-using v2rayN.ViewModels;
+using System.Windows.Controls;
 
 namespace v2rayN.Views
 {
-    // رفع ابهام UserControl با استفاده صریح از System.Windows.Controls.UserControl
-    public partial class QrcodeView : System.Windows.Controls.UserControl, IViewFor<QrcodeViewModel>
+    public partial class QrcodeView : System.Windows.Controls.UserControl
     {
         public QrcodeView()
         {
             InitializeComponent();
-            ViewModel = new QrcodeViewModel();
-
-            this.WhenActivated(disposables =>
-            {
-                this.OneWayBind(ViewModel, vm => vm.QrcodeSource, v => v.imgQrcode.Source).DisposeWith(disposables);
-            });
-        }
-
-        public QrcodeViewModel ViewModel
-        {
-            get => (QrcodeViewModel)GetValue(ViewModelProperty);
-            set => SetValue(ViewModelProperty, value);
-        }
-
-        public static readonly DependencyProperty ViewModelProperty =
-            DependencyProperty.Register("ViewModel", typeof(QrcodeViewModel), typeof(QrcodeView));
-
-        object IViewFor.ViewModel
-        {
-            get => ViewModel;
-            set => ViewModel = (QrcodeViewModel)value;
         }
     }
 }
